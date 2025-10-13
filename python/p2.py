60a6160e62c9e00f639c9440331f817434f9683f
Krishna-design396 <sini24362@gmail.com>
2025-10-13 15:46:41 +0530

    Add files via upload

---
+++ python/p2.py
@@ -0,0 +1,12 @@
+import datetime
+current_year = datetime.date.today().year 
+        # if today is 25 September 2025, the expression will return 2025
+# Get the final year from the user
+final_year = int(input("Enter the final year: "))
+if final_year < current_year:
+    print("Final year must be greater than or equal to the current year")
+else:
+    print (f"Leap years from {current_year} to {final_year}")
+    for year in range (current_year, final_year + 1):
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            print(year)
---
+++ python/pg1.py
@@ -0,0 +1,2 @@
+name="Krishnapriya"
+print(name)
