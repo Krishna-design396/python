c1a7262a192263f49171fd5085fc553509fdcf8c
Krishna-design396 <sini24362@gmail.com>
2025-10-15 15:56:02 +0530

    Add files via upload

---
+++ function/f2.py
@@ -0,0 +1,7 @@
+def modifyValue(a):
+    print("Value recieved in function:",a)
+    a=10
+    print("value inside function aftwr change:",a)
+a=5
+modifyValue(a)
+print("Value outside function:",a)
---
+++ function/f4.py
@@ -0,0 +1,5 @@
+def addNum(num1,num2):
+    sum = num1 + num2
+    print('Sum = :',sum)
+addNum(5,4)
+    
